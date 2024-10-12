661a14277f0373ef4544e4dffb366f395c52e186
K-sama <drumer19861018@gmail.com>
2024-10-13 01:35:37 +0900

    Added conditional in content files & added Images

--- parts/content-single.php
+++ parts/content-single.php
@@ -1,22 +1,26 @@
    <!-- 投稿のIDを出す the_ID() と post_class() -->
    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <header>
-            <h1><?php the_title(); ?></h1>
-            <?php
-            if (has_post_thumbnail()) {
-              the_post_thumbnail([560, 560], ['alt' => '']);
-            }
-            ?>
-            <div class="meta-info">
-              <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-              <p>Categories: <?php the_category(' '); ?></p>
-              <p>Tags: <?php the_tags('', ', '); ?></p>
-            </div>
-          </header>
-          <div class="content">
-            <?php the_content(); ?>
+     <header>
+       <h1><?php the_title(); ?></h1>
+       <?php
+        if (has_post_thumbnail()) {
+          the_post_thumbnail([560, 560], ['alt' => '']);
+        }
+        ?>
+       <div class="meta-info">
+         <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
+         <?php if (has_category()): ?>
+           <p>Categories: <?php the_category(' '); ?></p>
+         <?php endif; ?>
+         <?php if (has_tag()): ?>
+           <p>Tags: <?php the_tags('', ', '); ?></p>
+         <?php endif; ?>
+       </div>
+     </header>
+     <div class="content">
+       <?php the_content(); ?>
 
-            <!-- Page Break のページネーション -->
-             <?php wp_link_pages(); ?>
-          </div>
-        </article>
+       <!-- Page Break のページネーション -->
+       <?php wp_link_pages(); ?>
+     </div>
+   </article>
--- parts/content.php
+++ parts/content.php
@@ -2,13 +2,21 @@
 <article class="post">
   <a href="<?php echo get_the_permalink(); ?>">
     <h2><?php the_title(); ?></h2>
-    <?php // the_post_thumbnail('large'); 
-    ?>
+
+  <?php if (has_post_thumbnail()) : ?>
     <?php the_post_thumbnail(array(300, 300)); ?>
+  <?php else : ?>
+    <img src="<?php echo get_template_directory_uri(); ?>/images/IMG_7540.jpg" alt="">
+  <?php endif; ?>
+
     <div class="meta-info">
       <p>Posted in <?php echo get_the_date(); ?> by <?php the_author(); ?></p>
-      <p>Categories: <?php the_category(" | ") ?></p>
-      <p>Tags: <?php the_tags('', ', ') ?></p>
+      <?php if( has_category()): ?>
+            <p>Categories: <?php the_category( ' ' ); ?></p>
+        <?php endif; ?>
+        <?php if( has_tag()): ?>
+            <p>Tags: <?php the_tags( '', ', '); ?></p>
+        <?php endif; ?>
     </div>
     <p><?php the_excerpt(); ?></p>
   </a>
